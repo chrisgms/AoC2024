ecab028333c13347e2ef6136b071c204ae559bbe
Chris Gil Martins <cgilmartins@gmail.com>
2024-12-07 14:14:46 -0400

    day7

---
+++ day7/part1.py
@@ -0,0 +1,36 @@
+import itertools
+
+def multiply(a, b):
+   return a * b
+
+def add(a, b):
+   return a + b
+
+file = "./example1.txt"
+file = "./input1.txt"
+
+with open(file) as f:
+    equations = f.read().splitlines()
+
+    operators = [multiply, add]
+
+    total = 0
+    for eq in equations:
+        result, operands = eq.split(":")
+        result = int(result)
+        operands = list(map(int, operands.strip().split()))[::-1]
+      
+        operatorCombos = map(list, itertools.product(operators, repeat=len(operands) - 1))
+
+        for combo in operatorCombos:
+            nums = operands.copy()
+            while len(nums) > 1:
+                a = nums.pop()
+                b = nums.pop()
+                op = combo.pop()
+                nums.append(op(a,b))
+            if nums[0] == result:
+                total += result
+                break
+
+    print(total)
